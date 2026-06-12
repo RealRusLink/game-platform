446a56a410e63864a9fb28fe629997c77c81c02f
RealRusLink <real.rus.link@gmail.com>
2026-06-12 15:41:31 +0300

    feat: endpoint for getting all users

---
+++ Endpoints/Users.cs
@@ -0,0 +1,22 @@
+﻿using game_platform.Models;
+using game_platform.Repository;
+
+namespace game_platform.Endpoints;
+
+public class Users : IEndpointModule
+{
+    public void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/users");
+
+        group.MapGet("", getUsers);
+
+    }
+
+    private static async Task<IResult> getUsers(game_platform.Repository.User users)
+    {
+        return Results.Json(await users.GetAllUsers());
+    }
+    
+    
+}
