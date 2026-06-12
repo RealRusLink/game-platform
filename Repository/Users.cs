a61e9f5f18d58f2b7ef7703d8e0eba9d848d8873
RealRusLink <real.rus.link@gmail.com>
2026-06-12 15:33:51 +0300

    feat: repository for saving ang getting all app users

---
+++ Repository/Users.cs
@@ -0,0 +1,30 @@
+﻿using game_platform.Models;
+using MongoDB.Driver;
+
+namespace game_platform.Repository;
+
+public class User
+{
+    private readonly IMongoCollection<TelegramUser> _users;
+
+    public User(IMongoDatabase database)
+    {
+        _users = database.GetCollection<TelegramUser>("Users");
+    }
+
+    public async Task RegisterUserIfNotExists(TelegramUser user)
+    {
+        await _users.ReplaceOneAsync(
+            savedUser => savedUser.Id == user.Id,
+            user,
+            new ReplaceOptions { IsUpsert = true }
+        );
+    }
+
+    public async Task<List<TelegramUser>> GetAllUsers()
+    {
+        return await _users.Find(user => true).ToListAsync();
+    }
+    
+    
+}
